2384666e47865566ab923c1cc5af01723edf0977
Dj Walker-Morgan <codepope@gmail.com>
2022-08-08 20:33:31 +0100

    Initial Commit

---
+++ DNDButton/DNDButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DNDButtonApp.swift
+//  DNDButton
+//
+//  Created by Dj Walker-Morgan on 08/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct DNDButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
