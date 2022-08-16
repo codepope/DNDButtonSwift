2a69bc7d89bbc64b34476f4cc2ab9a98136b9f67
Dj Walker-Morgan <codepope@gmail.com>
2022-08-16 21:34:01 +0100

    Hackety hack - make unique client id and turn on autoreconnect

--- DNDButton/ContentView.swift
+++ DNDButton/ContentView.swift
@@ -9,46 +9,55 @@ import CocoaMQTT
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.scenePhase) var scenePhase
+  @Environment(\.scenePhase) var scenePhase
 
-    let mqttClient = CocoaMQTT(clientID: "dndbuttonswift", host: "jonah.local", port: 1883)
-
-    @State public var Status: Bool = false
-
-    var body: some View {
-      VStack(alignment: .leading) {
-        Image(systemName: Status ? "mic.fill" : "globe").resizable()
-              .foregroundColor(Status ? Color.red : Color.black).padding().frame(maxWidth: .infinity, maxHeight: .infinity).onTapGesture {
-              print("Clck")
-              self.mqttClient.publish("dnd/state", withString:!Status ? "1" : "0", qos: .qos1, retained: true)
-            
-        }
-    }.onAppear(perform: {
-      self.mqttClient.username = "dnd"
-      self.mqttClient.password = "dnd"
-      self.mqttClient.autoReconnect=true
-      self.mqttClient.keepAlive = 60
-      _ = self.mqttClient.connect()
-      self.mqttClient.didConnectAck = { _, _ in
-          self.mqttClient.subscribe("dnd/state")
-          self.mqttClient.didReceiveMessage = {
-              _, message, _ in Status = (message.string! as NSString).boolValue
+  let mqttClient = CocoaMQTT(clientID: "dndbuttonswift-" + String(ProcessInfo().processIdentifier), host: "jonah.local", port: 1883)
+  
+  @State public var Status: Bool = false
+  
+  var body: some View {
+    VStack(alignment: .leading) {
+      Image(systemName: Status ? "mic.fill" : "globe").resizable()
+        .foregroundColor(Status ? Color.red : Color.black).padding().frame(maxWidth: .infinity, maxHeight: .infinity)
+        .onTapGesture {
+          let newstatus = !Status ? "1" : "0"
+          print("Clck for \(newstatus)")
+          self.mqttClient.publish("dnd/state", withString:!Status ? "1" : "0", qos: .qos1, retained: true)
+          print(self.mqttClient.connState)
+        }.onAppear(perform: {
+          print("Appears")
+          self.mqttClient.username = "dnd"
+          self.mqttClient.password = "dnd"
+          self.mqttClient.autoReconnect=true
+          self.mqttClient.keepAlive = 60
+          _ = self.mqttClient.connect()
+          self.mqttClient.didConnectAck = { _, _ in
+            print("Connected")
+            self.mqttClient.subscribe("dnd/state")
+            self.mqttClient.didReceiveMessage = {
+              _, message, _ in
+              Status = (message.string! as NSString).boolValue
+              print(Status)
+            }
+            self.mqttClient.didDisconnect = {
+              _, error in print("Disconnected \(error)")
+            }
           }
-          print("Done")
-      }
-  })}
+        }
+        )
+    }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .frame(width: 500.0, height: 00.0)
-    }
+  static var previews: some View {
+    ContentView()
+      .frame(width: 500.0, height: 00.0)
+  }
 }
 
-
 //            Button(action: {
-//              
+//
 //            }, label: {
 //                HStack {
 //                    Text("Light")
